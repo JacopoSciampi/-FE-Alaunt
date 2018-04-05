6f46fce920ca5590bfccb10d086868dc4bf39feb
Jacopo <sciampi.jacopo@gmail.com>
2018-04-05 12:16:47 +0200

    ended first part building

--- src/app/game/core/service/home.service.ts
+++ src/app/game/core/service/home.service.ts
@@ -1,8 +1,16 @@
+import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
+
+import { GLOBAL_PATH } from './static';
 
 @Injectable()
 export class HomeService {
+    constructor(
+        private http: Http
+    ) { }
 
-  constructor() { }
+    public getRiepilogo(username: string) {
+        return this.http.get(GLOBAL_PATH + 'resources/home.php?username=' + username);
+    }
 
 }
--- src/app/game/main-pages/home/home.component.ts
+++ src/app/game/main-pages/home/home.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { HomeService } from '../../core/service/home.service';
+import { HomeModelsFromBE } from './home.models';
+import { GuardService } from '../../core/service/auth.guard';
+import { ToastService } from '../../core/service/toast.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+    public data: HomeModelsFromBE;
+    public isRequestLoading = true;
 
-  constructor() { }
+    constructor(
+        private ms: ToastService,
+        private guard: GuardService,
+        private service: HomeService,
+    ) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.guard.currentUser = 'jeko';
+        this.service.getRiepilogo(this.guard.currentUser)
+            .subscribe((res: any) => {
+                const data = JSON.parse(res._body) as HomeModelsFromBE;
+                if (!!data && data.status) {
+                    this.data = data;
+                    this.isRequestLoading = false;
+                } else {
+                    this.ms.error(data.message);
+                }
+            });
+    }
 
 }
